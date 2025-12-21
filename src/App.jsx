21dd9cb454d8cfc885af5c5b257b8efe1b15459a
Ahmed Sualih <sualihahmed26@gmail.com>
2025-12-21 09:59:04 +0300

    adding authentication

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path="/:userId/places" element={<UserPlaces />} />
         <Route path="/places/new" element={<NewPlace />} />
         <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="/auth" element={<Auth />} />
 
         {/* catch-all route */}
         <Route path="*" element={<Navigate to="/ahmed" replace />} />
--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,50 +1,75 @@
-import React, { useState } from 'react'
-import "./PlaceItem.css"
-import Button from '../../shared/components/FormElements/Button'
-import Card from '../../shared/components/UIElement/Card'
-import Modal from '../../shared/components/UIElement/Modal'
-import Map from '../../shared/components/UIElement/Map'
+import React, { useState } from "react";
+import "./PlaceItem.css";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElement/Card";
+import Modal from "../../shared/components/UIElement/Modal";
+import Map from "../../shared/components/UIElement/Map";
 const PlaceItem = (props) => {
-    const [showMap,setShowMap]=useState(false)
-    const openMapHandler=()=>{
-        setShowMap(true)
-    }
-    const closeMapHandler=()=>{
-        setShowMap(false)
-    }
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowconfirmModal] = useState(false);
+  const openMapHandler = () => {
+    setShowMap(true);
+  };
+  const closeMapHandler = () => {
+    setShowMap(false);
+  };
+  const showDeleteWarningHandler = () => {
+    setShowconfirmModal(true);
+  };
+  const cancelDeleteHandler = () => {
+    setShowconfirmModal(false);
+  };
+  const confirmDeleteHandler = () => {
+        setShowconfirmModal(false);
+    console.log("DELETING....");
+  };
   return (
     <React.Fragment>
-        <Modal
-         show={showMap}
-         onCancel={closeMapHandler}
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
         header={props.address}
         contentClass="place-item__modal-content"
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
-        >
-     <div className='map-container'>
-       <Map center={props.coordinates} zoom={16}/>
-     </div>
-             </Modal>
-      <li className='place-item'>
-        <Card className='place-item__content'>
-        <div className='place-item__image'>
-            <img src={props.image} alt={props.title} />
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
         </div>
-        <div className="place-item__info">
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button onClick={cancelDeleteHandler} inverse> CANCEl</Button>
+            <Button danger onClick={confirmDeleteHandler}> DELETE</Button>
+          </React.Fragment>
+        }
+      ></Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
             <h2>{props.title}</h2>
             <h3>{props.address}</h3>
             <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-            <Button invers onClick={openMapHandler}>VIEW ON MAP</Button>
+          </div>
+          <div className="place-item__actions">
+            <Button invers onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
-        </div>
+            <Button onClick={showDeleteWarningHandler} danger>DELETE</Button>
+          </div>
         </Card>
       </li>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default PlaceItem
+export default PlaceItem;
